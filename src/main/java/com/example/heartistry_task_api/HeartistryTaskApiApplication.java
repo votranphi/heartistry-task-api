f7073cae9012762b8d8ea3c2781b6e974c85d68a
Vo Tran Phi <songokupkj@gmail.com>
2024-10-30 00:50:49 +0700

    chore: add spring boot maven project

---
+++ src/main/java/com/example/heartistry_task_api/HeartistryTaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.heartistry_task_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartistryTaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartistryTaskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/heartistry_task_api/HeartistryTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heartistry_task_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartistryTaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
