fdd29ea1cc56e145798a74ea4d633f24f8d02171
Vo Tran Phi <songokupkj@gmail.com>
2024-11-14 00:00:28 +0700

    feat: add wordsets necessary files

---
+++ src/main/java/com/example/heartistry_task_api/WordSets/WordSetsRepository.java
@@ -0,0 +1,9 @@
+package com.example.heartistry_task_api.WordSets;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WordSetsRepository extends JpaRepository<WordSets, Integer> {
+
+}
