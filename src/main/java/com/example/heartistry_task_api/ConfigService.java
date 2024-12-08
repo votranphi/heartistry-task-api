a667a05444bc38d4b6377d11cd82e2780b3664f9
Vo Tran Phi <songokupkj@gmail.com>
2024-12-08 12:01:46 +0700

    feat: add config service

---
+++ src/main/java/com/example/heartistry_task_api/ConfigService.java
@@ -0,0 +1,14 @@
+package com.example.heartistry_task_api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConfigService {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    public String getJwtSecret() {
+        return jwtSecret;
+    }
+}
