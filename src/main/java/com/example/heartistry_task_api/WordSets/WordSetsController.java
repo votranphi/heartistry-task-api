07d1e210e87db12f9c2b8dd1d042fb664236ee76
Vo Tran Phi <songokupkj@gmail.com>
2024-12-12 13:55:28 +0700

    feat(hta-01): patch and delete wordset endpoints

--- src/main/java/com/example/heartistry_task_api/WordSets/WordSetsController.java
+++ src/main/java/com/example/heartistry_task_api/WordSets/WordSetsController.java
@@ -3,19 +3,25 @@ package com.example.heartistry_task_api.WordSets;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.heartistry_task_api.Responses.Detail;
 import com.example.heartistry_task_api.Responses.ObjectWithPagination;
 import com.example.heartistry_task_api.WordSets.Dto.AddDto;
+import com.example.heartistry_task_api.WordSets.Dto.UpdateDto;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 
@@ -40,6 +46,20 @@ public class WordSetsController {
         );
 
         return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{id}")
+    public @ResponseBody ResponseEntity<WordSet> updateById(@PathVariable Integer id, @RequestBody UpdateDto updateDto) {
+        WordSet wordSet = wordSetsService.updateById(id, updateDto).get();
+
+        return ResponseEntity.ok(wordSet);
+    }
+
+    @DeleteMapping("/{id}")
+    public @ResponseBody ResponseEntity<Detail> deleteById(@PathVariable Integer id) {
+        wordSetsService.deleteWordById(id);
+
+        return new ResponseEntity<Detail>(new Detail("Delete wordset successfully", 200), HttpStatusCode.valueOf(200));
     }
 
     @GetMapping("/all")
--- src/main/java/com/example/heartistry_task_api/WordSets/WordSetsService.java
+++ src/main/java/com/example/heartistry_task_api/WordSets/WordSetsService.java
@@ -1,12 +1,16 @@
 package com.example.heartistry_task_api.WordSets;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.heartistry_task_api.WordSets.Dto.UpdateDto;
 
 @Service
 public class WordSetsService {
@@ -25,6 +29,18 @@ public class WordSetsService {
     public Page<WordSet> getSequenceOfPost(Integer idUser, Integer page, Integer pageSize) {
         Pageable pageable = PageRequest.of(page, pageSize);
         return wordSetsRepository.findByIdUser(idUser, pageable);
+    }
+
+    @Transactional
+    public Optional<WordSet> updateById(Integer id, UpdateDto updateDto) {
+        return wordSetsRepository.findById(id).map(target -> {
+            target.setTopic(updateDto.getTopic());
+            return target;
+        });
+    }
+
+    public void deleteWordById(Integer id) {
+        wordSetsRepository.deleteById(id);
     }
 
     public Integer countUserWordSet(Integer idUser) {
